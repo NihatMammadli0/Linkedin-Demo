2ea265450702f15e95f776f9a0b1d36e31d1dfd4
Nihat Mammadli <nihatmammadli42@gmail.com>
2023-12-18 21:04:21 +0400

    scrolling without smooth effect

---
+++ index.js
@@ -0,0 +1,15 @@
+window.addEventListener('scroll', (event) => {
+  // Get the number of pixels scrolled vertically
+  const scrollPosition = document.documentElement.scrollTop;
+  const doYouKnowPart = document.querySelector(".do-you-know-part");
+  // console.log(doYouKnowPart);  
+
+  // Adjust styles based on the scroll position
+  if (scrollPosition > 400) {
+    doYouKnowPart.style.visibility = 'hidden';
+  } else if(scrollPosition < 400) {
+    doYouKnowPart.style.visibility = 'visible';
+    // doYouKnowPart.style.transition = 'visibility ease 1s';
+  }
+});
+
